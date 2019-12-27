35a9f8b9f065feca113fa348217ed08813551665
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2019-12-26 23:29:16 -0300

    Update setup to WSLv2 and Vcxsrv 1.2.5.1

--- vcxsrv_setup.bat
+++ vcxsrv_setup.bat
@@ -1,3 +1,3 @@
-bash -c "wget https://downloads.sourceforge.net/project/vcxsrv/vcxsrv/1.19.3.4/vcxsrv-64.1.19.3.4.installer.exe"
-bash -c "echo export DISPLAY=:0 >> ~/.bashrc" 
-vcxsrv-64.1.19.3.4.installer.exe /s
+bash -c "wget https://ufpr.dl.sourceforge.net/project/vcxsrv/vcxsrv/1.20.5.1/vcxsrv-64.1.20.5.1.installer.exe"
+bash -c "echo export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0 >> ~/.test" 
+vcxsrv-64.1.20.5.1.installer.exe /s
