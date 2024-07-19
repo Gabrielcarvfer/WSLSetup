890cda10249a6370e7b7c98b8d3c2b877c0529b4
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2024-07-19 22:55:25 +0200

    Update ssh_setup.sh

--- ssh_setup.sh
+++ ssh_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo rm -rf /etc/apt/apt.conf.d/20snapd.conf
 sudo apt install openssh-server
 sudo dpkg-reconfigure openssh-server
 sudo cp /etc/ssh/sshd_config ./backup/etc/ssh/sshd_config
